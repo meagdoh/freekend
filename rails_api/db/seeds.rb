71affa85f6aafc293944af3a5d5ef1c0577a2300
meagdoh <meagdoherty@gmail.com>
2017-05-11 16:33:31 -0400

    Initial seed data

--- rails_api/db/seeds.rb
+++ rails_api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  city1 = City.create([{ name: 'Beverley Hills' }, { zip: '90210' }, { long: '0' },{ lat: '0' }])
+  category1 = Category.create([{ name_cat: 'Concert' }])
