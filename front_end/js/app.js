98e93232a87b88deb867cc6ce90eba54a50e76c2
Jonathan Wright <jonwright.va@gmail.com>
2017-05-12 10:03:32 -0400

    ng controller added and functioning

--- front_end/js/app.js
+++ front_end/js/app.js
@@ -2,11 +2,35 @@
 
 (function(){
 
+	let events = [{
+		name: "Party at my Place",
+		time: "7:30pm",
+		city: "Washington",
+		state: "DC",
+		zip_code: 20006,
+		address: "2343 Connecticut Street",
+		photo_url: "http://i.imgur.com/TbjJwFj.jpg"
+	}, {
+		name: "Tycho Show",
+		time: "9:30pm",
+		city: "Washington",
+		state: "DC",
+		zip_code: 20006,
+		address: "6364 West Street",
+		photo_url: "http://i.imgur.com/MkLTfuc.jpg"
+	}]
+
 angular
 	.module("freekend", [
 		"ui.router",
 		"ngResource"
 	])
-	.controller()
+	.controller("freekendController", [
+		FreekendControllerFunction
+	])
+
+	function FreekendControllerFunction() {
+		this.event_list = events
+	}
 
 })();
