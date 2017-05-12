4ba4b854fec877aa54df08af32da48f945addf97
meagdoh <meagdoherty@gmail.com>
2017-05-12 10:40:17 -0400

    Add commment_controller

--- rails_api/app/controllers/comments_controller.rb
+++ rails_api/app/controllers/comments_controller.rb
@@ -1,24 +1,35 @@
 class CommentsController < ApplicationController
 
-def index
-end
+    def index
+      @comments = Comment.all
+      render json: @coments
+    end
 
-def show
-end
+    def show
+      @comment = comment.find(params[:id])
+      render json: @comment
+    end
 
-def new
-end
+    def create
+      @comment = comment.create!(comment_params)
+      render json: @comment
+    end
 
-def create
-end
+    def update
+      @comment = comment.find(params[:id])
+      @comment.update!(comment_params)
+      render json: @comment
+    end
 
-def edit
-end
+    def destroy
+      @comment = comment.find(params[:id])
+      @comment.destroy
+      render nothing:true
+    end
 
-def update
-end
-
-def delete
-end
+    private
+    def comment_params
+      params.require(:comment).permit(:author, :content, :event_id)
+    end
 
 end
