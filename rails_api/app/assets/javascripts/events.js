fbc10f542cccbd3496f9ef0bedb33c7afa501467
meagdoh <meagdoherty@gmail.com>
2017-05-16 18:21:07 -0400

    Add city seed data

---
+++ rails_api/app/assets/javascripts/events.js
@@ -0,0 +1,140 @@
+"use strict";
+
+(function() {
+    let eventUrl = ""
+    let zip = 0
+    let eventsList = ""
+    let singleEvent = ""
+
+    angular
+        .module("freekend", [
+            "ui.router",
+            "ngResource",
+            'ngSanitize'
+        ])
+        .controller("FreekendController", [
+            FreekendControllerFunction
+        ])
+        .config([
+            "$stateProvider",
+            RouterFunction
+        ])
+        .factory("LocationFactory", [
+            "$resource",
+            LocationFactoryFunction
+        ])
+        .factory("EventFactory", [
+            "$resource",
+            EventFactoryFunction
+        ])
+        .factory("CommentFactory", [
+          "$resource",
+          CommentFactoryFunction
+        ])
+        .controller("FreekendIndexController", [
+            "LocationFactory",
+            "EventFactory",
+            FreekendIndexControllerFunction
+        ])
+        .controller("FreekendShowController", [
+            "$stateParams",
+            "LocationFactory",
+            "EventFactory",
+            "CommentFactory",
+            FreekendShowControllerFunction
+        ])
+        .controller("FreekendCommentNewController",[
+          "$stateParams",
+          "CommentFactory",
+          FreekendCommentNewControllerFunction
+      ])
+
+    function RouterFunction($stateProvider) {
+        $stateProvider
+            .state("eventsIndex", {
+                url: "/",
+                templateUrl: "js/ng-views/index.html",
+                controller: "FreekendIndexController",
+                controllerAs: "vm"
+            })
+            .state("eventShow", {
+                url: "/event/:id",
+                templateUrl: "js/ng-views/show.html",
+                controller: "FreekendShowController",
+                controllerAs: "vm"
+            })
+            .state("commentNew", {
+              url: "/event/:id/comment/new",
+              templateUrl:  "js/ng-views/comments/new.html",
+              controller: "FreekendCommentNewController",
+              controllerAs: "vm"
+            })
+    }
+
+    function FreekendControllerFunction() {
+    }
+
+    function FreekendIndexControllerFunction(LocationFactory, EventFactory) {
+        let self = this
+        this.events
+        this.event_description
+        LocationFactory.get().$promise.then(function(response) {
+            self.zip = response.zip
+            console.log(self.zip)
+            EventFactory.get({
+                zip: response.zip
+            }).$promise.then(function(data) {
+                self.events = data.events.event
+                eventsList = data.events.event
+            })
+        })
+
+        this.setSingleEvent = function(place) {
+            singleEvent = place
+            console.log(singleEvent)
+            console.log("click")
+        }
+    }
+
+    function FreekendShowControllerFunction($stateParams, LocationFactory, EventFactory, CommentFactory) {
+       let self = this
+       this.eventId = $stateParams.id
+
+
+        LocationFactory.get().$promise.then(function(response) {
+            EventFactory.get({zip: response.zip}).$promise.then(function(data) {
+                self.events = data.events.event
+                eventsList = data.events.event
+                console.log(self.events)
+                CommentFactory.query().$promise.then(function(data2){
+                   self.comments = data2
+                })
+            })
+        })
+    }
+
+    function FreekendCommentNewControllerFunction($stateParams, CommentFactory){
+      this.comment = new CommentFactory()
+      console.log($stateParams.id)
+      this.comment.event_id = $stateParams.id
+      this.create = function (){
+        this.comment.$save()
+      }
+    }
+
+
+    function LocationFactoryFunction($resource) {
+        return $resource("http://ip-api.com/json")
+    }
+
+    function EventFactoryFunction($resource) {
+        return $resource("http://api.eventful.com/json/events/search?!sort=rec&location=:zip&date=Today&app_key=N6hhj9BZcLjg2KmX")
+    }
+
+    function CommentFactoryFunction ($resource) {
+        return $resource("http://localhost:3000/comments")
+    }
+
+
+
+})();
--- rails_api/db/seeds.rb
+++ rails_api/db/seeds.rb
@@ -12,6 +12,10 @@ Comment.destroy_all
 Favorite.destroy_all
 
   city1 = City.create([{ name: 'Beverley Hills' }, { zip: '90210' }, { long: '0' },{ lat: '0' }])
+  city2 = City.create([{ name: 'Boston' })
+  city3 = City.create([{ name: 'Chicago' })
+  city4 = City.create([{ name: 'Washington' })
+
   category1 = Category.create([{ name_cat: 'Concert' }])
   comment1 = Comment.create([{author: 'Stormy', content: 'This concert is going to be ah-maze-ing' , event_id: "1"}])
   comment2 = Comment.create([{author: 'Jon', content: 'This concert is going to be horrible' , event_id: "1"}])
